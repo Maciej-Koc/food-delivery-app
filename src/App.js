278062f7df673c06cdb9ef810d192f32f018322d
Maciej-Koc <mkoc2@asu.edu>
2022-07-17 20:45:47 -0500

    Completed close cart button and closing cart in the backdrop area.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-// import "./App.css";
 import React from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = React.useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
