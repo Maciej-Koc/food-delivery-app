4baa9af6b8db88bb9ddec570c8ace4ab024dc502
Maciej-Koc <mkoc2@asu.edu>
2022-07-23 22:19:58 -0500

    updated AvailableMeals to fetch from database for meals available.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,33 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 export default function AvailableMeals(props) {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const mealArr = [];
+    fetch("https://meal-639c1-default-rtdb.firebaseio.com/meals.json")
+      .then((response) => response.json())
+      .then((data) => {
+        for (const key in data) {
+          mealArr.push({
+            id: key,
+            key: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        console.log("meal array", mealArr);
+        setMeals(mealArr);
+      });
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id} // this is new!
       key={meal.id}
